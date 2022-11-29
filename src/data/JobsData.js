32879a050b0f1948ab4f68aeb147d35a326621ef
fares <fares12yaseer45@gmail.com>
2022-11-29 20:08:10 +0200

    create data file and add some data

---
+++ src/data/JobsData.js
@@ -0,0 +1,131 @@
+export const JobsData = [
+    {
+        title: 'Front End Developer',
+        desc: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications',
+        description: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications. You should be able to translate our company and customer needs into functional and appealing interactive applications. If youre interested in creating a user-friendly environment by writing code and moving forward in your career, then this job is for you. We expect you to be a tech-savvy professional, who is curious about new digital technologies and aspires to combine usability with visual design.Ultimately, you should be able to create a functional and attractive digital environment for our company, ensuring great user experience.',
+        employer: 'Josehp Markos',
+        company: 'Niyo'
+    },
+    {
+        title: 'Back End Developer',
+        desc: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs',
+        description: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs They make sure the website is scalable and that it can still function when hit by large loads—be that traffic or demanding scripts. They are also tasked with maintaining and testing existing back end elements to ensure they are as fast and efficient as possible. Data storage also comes under their remit, which requires them to have a good knowledge of data security and compliance.',
+        employer: 'Better Dany',
+        company: 'DigiCupid'
+    },
+    {
+        title: 'Full Stack Developer',
+        desc: 'Full stack developers are computer programmers who are proficient in both front and back end coding.',
+        description: 'We are looking for a highly skilled computer programmer who is comfortable with both front and back end programming Full stack developers are responsible for developing and designing front end web architecture, ensuring the responsiveness of applications, and working alongside graphic designers for web design features, among other duties.',
+        employer: 'Fares Yasser',
+        company: 'Primida'
+    },
+    {
+        title: 'UI UX Designer',
+        desc: 'We are looking for a UI/UX Designer to turn our software into easy-to-use products for our clients.',
+        description: 'UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components To be successful in this role, you should have experience with design software and wireframe tools. If you also have a portfolio of professional design projects that includes work with web/mobile applications, would like to meet you.',
+        employer: 'Liam Oliver',
+        company: 'Tidal Wave'
+    },
+    {
+        title: 'IOS Developer',
+        desc: 'iOS developers design and build applications for mobile devices running APPLE iOS operating software',
+        description: 'We are looking to hire a talented iOS developer to design, build, and maintain the next generation of iOS applications Your primary focus will be developing high-end iOS applications for the latest Apple mobile devices Your duties may include collaborating with the design team for new application features, identifying and fixing application bottlenecks, maintaining the core code, and updating applications published on the App Store.',
+        employer: 'Elijah James',
+        company: 'Warrior'
+    },
+    {
+        title: 'ANDROID Developer',
+        desc: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits',
+        description: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits This Android app developer will work with our team of talented engineers to design and build the next generation of our mobile applications. Android programming works closely with other app development and technical teams.',
+        employer: 'William Benjamin',
+        company: 'Drive '
+    },
+    {
+        title: 'Front End Developer',
+        desc: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications',
+        description: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications. You should be able to translate our company and customer needs into functional and appealing interactive applications. If youre interested in creating a user-friendly environment by writing code and moving forward in your career, then this job is for you. We expect you to be a tech-savvy professional, who is curious about new digital technologies and aspires to combine usability with visual design.Ultimately, you should be able to create a functional and attractive digital environment for our company, ensuring great user experience.',
+        employer: 'Josehp Markos',
+        company: 'Niyo'
+    },
+    {
+        title: 'Back End Developer',
+        desc: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs',
+        description: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs They make sure the website is scalable and that it can still function when hit by large loads—be that traffic or demanding scripts. They are also tasked with maintaining and testing existing back end elements to ensure they are as fast and efficient as possible. Data storage also comes under their remit, which requires them to have a good knowledge of data security and compliance.',
+        employer: 'Better Dany',
+        company: 'DigiCupid'
+    },
+    {
+        title: 'Full Stack Developer',
+        desc: 'Full stack developers are computer programmers who are proficient in both front and back end coding.',
+        description: 'We are looking for a highly skilled computer programmer who is comfortable with both front and back end programming Full stack developers are responsible for developing and designing front end web architecture, ensuring the responsiveness of applications, and working alongside graphic designers for web design features, among other duties.',
+        employer: 'Fares Yasser',
+        company: 'Primida'
+    },
+    {
+        title: 'UI UX Designer',
+        desc: 'We are looking for a UI/UX Designer to turn our software into easy-to-use products for our clients.',
+        description: 'UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components To be successful in this role, you should have experience with design software and wireframe tools. If you also have a portfolio of professional design projects that includes work with web/mobile applications, would like to meet you.',
+        employer: 'Liam Oliver',
+        company: 'Tidal Wave'
+    },
+    {
+        title: 'IOS Developer',
+        desc: 'iOS developers design and build applications for mobile devices running APPLE iOS operating software',
+        description: 'We are looking to hire a talented iOS developer to design, build, and maintain the next generation of iOS applications Your primary focus will be developing high-end iOS applications for the latest Apple mobile devices Your duties may include collaborating with the design team for new application features, identifying and fixing application bottlenecks, maintaining the core code, and updating applications published on the App Store.',
+        employer: 'Elijah James',
+        company: 'Warrior'
+    },
+    {
+        title: 'ANDROID Developer',
+        desc: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits',
+        description: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits This Android app developer will work with our team of talented engineers to design and build the next generation of our mobile applications. Android programming works closely with other app development and technical teams.',
+        employer: 'William Benjamin',
+        company: 'Drive '
+    },
+    {
+        title: 'Front End Developer',
+        desc: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications',
+        description: 'We are looking for a qualified Front-end developer to join our IT team. You will be responsible for building the client-side of our web applications. You should be able to translate our company and customer needs into functional and appealing interactive applications. If youre interested in creating a user-friendly environment by writing code and moving forward in your career, then this job is for you. We expect you to be a tech-savvy professional, who is curious about new digital technologies and aspires to combine usability with visual design.Ultimately, you should be able to create a functional and attractive digital environment for our company, ensuring great user experience.',
+        employer: 'Josehp Markos',
+        company: 'Niyo'
+    },
+    {
+        title: 'Back End Developer',
+        desc: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs',
+        description: 'A backend developer works alongside front end developers, full stack developers, programmers or UX specialists to create comprehensive digital solutions for business needs They make sure the website is scalable and that it can still function when hit by large loads—be that traffic or demanding scripts. They are also tasked with maintaining and testing existing back end elements to ensure they are as fast and efficient as possible. Data storage also comes under their remit, which requires them to have a good knowledge of data security and compliance.',
+        employer: 'Better Dany',
+        company: 'DigiCupid'
+    },
+    {
+        title: 'Full Stack Developer',
+        desc: 'Full stack developers are computer programmers who are proficient in both front and back end coding.',
+        description: 'We are looking for a highly skilled computer programmer who is comfortable with both front and back end programming Full stack developers are responsible for developing and designing front end web architecture, ensuring the responsiveness of applications, and working alongside graphic designers for web design features, among other duties.',
+        employer: 'Fares Yasser',
+        company: 'Primida'
+    },
+    {
+        title: 'UI UX Designer',
+        desc: 'We are looking for a UI/UX Designer to turn our software into easy-to-use products for our clients.',
+        description: 'UI/UX Designer responsibilities include gathering user requirements, designing graphic elements and building navigation components To be successful in this role, you should have experience with design software and wireframe tools. If you also have a portfolio of professional design projects that includes work with web/mobile applications, would like to meet you.',
+        employer: 'Liam Oliver',
+        company: 'Tidal Wave'
+    },
+    {
+        title: 'IOS Developer',
+        desc: 'iOS developers design and build applications for mobile devices running APPLE iOS operating software',
+        description: 'We are looking to hire a talented iOS developer to design, build, and maintain the next generation of iOS applications Your primary focus will be developing high-end iOS applications for the latest Apple mobile devices Your duties may include collaborating with the design team for new application features, identifying and fixing application bottlenecks, maintaining the core code, and updating applications published on the App Store.',
+        employer: 'Elijah James',
+        company: 'Warrior'
+    },
+    {
+        title: 'ANDROID Developer',
+        desc: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits',
+        description: 'We are looking for an Android Developer who possesses a passion for pushing mobile technologies to the limits This Android app developer will work with our team of talented engineers to design and build the next generation of our mobile applications. Android programming works closely with other app development and technical teams.',
+        employer: 'William Benjamin',
+        company: 'Drive '
+    },
+]
+
+
+
