9588cb08497e6046d787668e3565d37b4cc8ae8a
fares <fares12yaseer45@gmail.com>
2022-11-29 02:01:04 +0200

    just a modifie

--- src/components/Join/JoinPage.jsx
+++ src/components/Join/JoinPage.jsx
@@ -41,6 +41,8 @@
                 flex: "2",
             }}
             placeholder="...@gmail.com"
+            type='email'
+            required
             />
             <Button
             sx={{
